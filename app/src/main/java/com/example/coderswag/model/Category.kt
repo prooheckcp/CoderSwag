7b85a5a993ef25b80c599a2ed1660e8a280874ae
prooheckcp <vasco.soares.2001@gmail.com>
2020-12-01 19:10:54 +0000

    Data Structure

---
+++ app/src/main/java/com/example/coderswag/model/Category.kt
@@ -0,0 +1,3 @@
+package com.example.coderswag.model
+
+class Category constructor(val title: String, val image: String)
---
+++ app/src/main/java/com/example/coderswag/model/Product.kt
@@ -0,0 +1,3 @@
+package com.example.coderswag.model
+
+class Product constructor(val title: String, val price: String, val image: String)
